b5dcbf1c989f21b8146bb2426930a0a6923e3bfd
Julia Kalvik <julia.kalvik@yahoo.no>
2024-04-05 16:53:25 +0200

    Improved contact form design

--- app/contact/page.js
+++ app/contact/page.js
@@ -2,14 +2,15 @@ export default function Contact() {
     return (
     <>
     <h1>contact us</h1>
-    <div className="antialiased bg-gray-100">
-        <div className="flex w-full min-h-screen justify-center items-center">
-            <div className="flex flex-col md:flex-row md:space-x-6 space-y-6 md:space-y-0 bg-cyan-700 w-full max-w-6xl p-8 rounded-xl shadow-lg text-white">
-                <div className="flex flex-col space-y-8 justify-between">
+    <div className="antialiased bg-gray-100 justify-center">
+        <div className="flex flex-col md:flex-col  min-h-screen justify-center items-center">
+            <div className="flex flex-col space-y-6 bg-cyan-700 w-full max-w-3xl p-8 rounded-xl shadow-lg text-white overflow-hidden">
+                <div className="flex flex-col md:flex-row md:space-x-6 space-y-8 md:space-y-0 justify-between items-center">
+                    <div className="flex flex-col space-y-8 justify-between p-4">
                     <div>
                         <h1 className="font-bold text-4xl tracking-wide">SAY HI</h1>
                         <p className="pt-4 pb-4 text-cyan-100 text-sm">
-                            lorem ipsum tralala lorem ipsum tralala, lorem
+                            lorem ipsum tralala lorem ipsum tralala, <br/> lorem lorem ipsum tralala lorem ipsum tralala, lorem 
                         </p>
                     </div>
                     <div className="flex flex-col space-y-6 pt-4">
@@ -25,6 +26,11 @@ export default function Contact() {
                         <i class="fa-solid fa-location-dot text-teal-300"></i>
                         <span>Noroff Road 45B</span>
                         </div>
+                    </div>
+                    <div className="relative">
+                        <div className=" absolute z-0 w-40 h-40 bg-teal-400 rounded-full right-60 -bottom-70"></div>
+                    </div>
+
                     </div>
                     <div className="bg-white rounded-xl shadow-lg p-8 text-gray-600">
                         <form action="" className="flex flex-col space-y-4 md:w-full lg:w-64">
