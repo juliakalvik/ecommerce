1407464a121c6f72fd9f2b89abd85b3f22262aaf
Julia Kalvik <julia.kalvik@yahoo.no>
2024-04-06 11:01:11 +0200

    Trying to solve param issues

--- app/Card.js
+++ app/Card.js
@@ -1,6 +1,8 @@
 import React from "react";
+import { useRouter } from "next/navigation";
+import useCart from "./(store)/store";
 
-export default function Card(props) {
+export default function Card({product}) {
     const { title, description, price, imageUrl, id } = product;
     
     return (
--- app/products/[productId]/page.js
+++ app/products/[productId]/page.js
@@ -2,15 +2,19 @@
 
 import { useEffect, useState } from 'react';
 import useCart from "../../(store)/store";
+import { useRouter } from 'next/navigation';
 
-function ProductPage({ params }) {
+function ProductPage({ productId }) {
     const [product, setProduct] = useState(null);
     const cartProduct = useCart(state => state.product);
+    const router = useRouter();
 
     useEffect(() => {
+     
+        
         async function fetchProduct(id) {
             try {
-                const res = await fetch(`https://v2.api.noroff.dev/online-shop${id}`);
+                const res = await fetch(`https://v2.api.noroff.dev/online-shop/${id}`);
                 const data = await res.json();
                 setProduct(data);
             } catch (error) {
@@ -18,8 +22,12 @@ function ProductPage({ params }) {
             }
         }
 
-        fetchProduct(params.id);
-    }, [params.id]);
+        if (productId) {
+            fetchProduct(productId);
+        } else {
+            console.log('productId is missing');
+        }
+    }, [productId]); 
 
     return (
         <div>
@@ -27,7 +35,9 @@ function ProductPage({ params }) {
                 <h1>details {product.id}</h1>
             ) : (
                 <p>Loading...</p>
+                
             )}
+           
         </div>
     );
 }
